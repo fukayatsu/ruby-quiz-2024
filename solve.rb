a9a8e33fb4a998dc1a347631d4f16962f603fdf6
fukayatsu <fukayatsu@gmail.com>
2024-05-17 13:51:54 +0900

    spike 4

--- solve.rb
+++ solve.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require "open3"
 
-expected_error, original_code = File.read('5.txt').split('---')
+expected_error, original_code = File.read("#{ARGV[0]}.txt").split('---')
 
 `rm -f tmp/*`
 
@@ -20,11 +20,11 @@ end
 
 threads = []
 0.upto original_code.length do |i|
-  threads << Thread.start(i) do |i|
-    (" "..."~").each do |char|
-      code = original_code[0...i].to_s + char + original_code[i..-1].to_s
-      File.write("tmp/temp__add_#{i}.rb", code)
-      o, e, s = Open3.capture3("ruby tmp/temp__add_#{i}.rb")
+  threads << Thread.start(i) do |t|
+    (["\n"] + (" "..."~").to_a).each do |char|
+      code = original_code[0...t].to_s + char + original_code[i..-1].to_s
+      File.write("tmp/temp__add_#{t}.rb", code)
+      o, e, s = Open3.capture3("ruby tmp/temp__add_#{t}.rb")
       if e.include? expected_error.strip
         puts "!!!solved!!!"
         puts code
